f546371129726d582ef5e3a854b6bd374bef8457
Анна Потапчук <potapchuk@dlit.dp.ua>
2022-02-19 23:55:31 +0200

    models done

---
+++ Backend/SmartCityKyiv/Models/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartCityKyiv.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime PublicationDate { get; set; }
+
+    }
+}
