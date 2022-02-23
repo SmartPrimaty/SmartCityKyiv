a9853759d1f3313be8b93e695c222aea19b09fc6
MrTimeGo <mrtimego@ukr.net>
2022-02-23 20:58:53 +0200

    created admin and moderator roles

---
+++ Backend/SmartCityKyiv/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartCityKyiv.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartCityKyiv.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SiteContext context;
+
+        public AccountController(SiteContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
--- Backend/SmartCityKyiv/Data/SiteContext.cs
+++ Backend/SmartCityKyiv/Data/SiteContext.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SmartCityKyiv.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SmartCityKyiv.Data
 {
@@ -26,7 +28,54 @@ namespace SmartCityKyiv.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId);
+
+            modelBuilder.Entity<Role>()
+                .HasMany(r => r.Users)
+                .WithOne(u => u.Role);
+
+            var roles = new Role[]
+            {
+                new Role()
+                {
+                    Id = 1,
+                    Name = "admin",
+                },
+                new Role()
+                {
+                    Id = 2,
+                    Name = "moderator"
+                }
+            };
+
+            var password = CreateHash("admin");
+            var admin = new User()
+            {
+                Email = "admin@smartcitykyiv.ua",
+                Password = password,
+                Name = "Administrator",
+                Role = roles[0],
+                RoleId = 1
+            };
+
+            modelBuilder.Entity<Role>()
+                .HasData(roles);
+
+            modelBuilder.Entity<User>()
+                .HasData(admin);
+        }
+
+
+        private string CreateHash(string source)
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] sourceBytes = Encoding.UTF8.GetBytes(source);
+            byte[] targerBytes = sha256.ComputeHash(sourceBytes);
+            string target = Encoding.UTF8.GetString(targerBytes);
+            return target;
         }
     }
 }
