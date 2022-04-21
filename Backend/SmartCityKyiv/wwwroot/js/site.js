8249712bef3a08936af1719b31bd45ad07b8eabb
grabelka <nastyusha.gr@gmail.com>
2022-04-21 23:47:39 +0300

    new header and slideshow added

--- Backend/SmartCityKyiv/wwwroot/js/site.js
+++ Backend/SmartCityKyiv/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var slideIndex = 1
+showSlides(slideIndex)
+timeInterval()
+
+function plusSlides(n) {
+  showSlides(slideIndex += n)
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n)
+}
+
+function timeInterval() {
+  setInterval(() => plusSlides(1), 5000)
+}
+
+function showSlides(n) {
+  var i = 0
+  var slides = document.getElementsByClassName("fade");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
